604e0c3816761fbbc851bb31960ff929e1fa6132
Dintoz254 <shadrackolungo@gmail.com>
2022-04-17 02:51:32 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,12 +21,6 @@ int main(void)
 		} else if (i % 3 != 0 && i % 5 == 0)
 		{
 			printf(" Buzz");
-		} else if (i == 1)
-		{
-			printf("%d", i);
-		} else if (i == 100)
-		{
-			printf(" Buzz");
 		} else
 		{
 			printf("%d", i);
