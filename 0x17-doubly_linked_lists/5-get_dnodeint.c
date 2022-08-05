33408e25d5b1905acd67c2910ea0f5f906accf61
dintoz254 <shadrackolungo@gmail.com>
2022-08-04 23:37:31 -0700

    get dnode

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -4,7 +4,7 @@
  * get_dnodeint_at_index - Get the nth node of the list
  * @head: Head of the list
  * @index: Index of the list, starting with 0
- * 
+ *
  * Return: Node in the current index
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
